e0b885c8b3d2f564b119f4ab9b633d4649b28a7c
Linus Phan <phanlinus@gmail.com>
2019-07-09 13:00:33 -0700

    create Model and Collection

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+const App = {
+  albumsLoaded: function () {},
+
+  fetchAlbums: function () {
+    this.albums = new Albums();
+    this.albums.fetch({
+      success: this.albumsLoaded.bind(this),
+    });
+  },
+
+  init: function () {
+    this.fetchAlbums();
+  },
+};
