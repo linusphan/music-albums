4516cff599c89303acc1aa526feaf54697801786
Linus Phan <phanlinus@gmail.com>
2019-07-10 15:04:22 -0700

    add test coverage

---
+++ public/integration/spec/tracks_view_spec.js
@@ -0,0 +1,32 @@
+jQuery.fx.off = true;
+
+describe('Tracks view', function () {
+  const album = albumsScaffold.findWhere({ title: 'Unbreakable Smile (Bonus Track Version)' });
+
+  beforeEach(function () {
+    this.view = new TracksView({ collection: tracksScaffold, album: album });
+  });
+
+  afterEach(function () {
+    this.view.remove();
+  });
+
+  it('has a collection property assigned', function () {
+    expect(this.view.collection).toBeDefined();
+    expect(this.view.collection.length).toBe(tracksScaffold.length);
+  });
+
+  it('has a Handlebars template compiled', function () {
+    expect(this.view.template).toBeDefined();
+  });
+
+  it('renders a modal to the body when render called', function () {
+    this.view.render();
+    expect($('#tracks_modal li').length).toBe(tracksScaffold.length);
+  });
+
+  it('removes the view when fadeOut called', function () {
+    this.vsiew.fadeOut();
+    expect($('#tracks_modal').length).toBe(0);
+  });
+});
